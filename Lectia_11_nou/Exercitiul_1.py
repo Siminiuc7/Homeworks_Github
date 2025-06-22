06380746f23df6331d8ad9e144eb6b4da47eafac
Siminiuc <siminiuc.rodica@gmail.com>
2025-06-22 19:07:46 +0200

    Functii-Lista_numere

---
+++ Lectia_11_nou/Exercitiul_1.py
@@ -0,0 +1,14 @@
+"""
+Task: Creați o funcție cu numele "task_1" care va returna o listă cu numerele de la 1 la 10
+Utilizați list comprehension.
+"""
+
+from math import factorial
+
+# CODUL TĂU VINE MAI JOS:
+def task_1():
+    return [x for x in range(1,11)]
+
+print(task_1())
+    
+# CODUL TĂU VINE MAI SUS
